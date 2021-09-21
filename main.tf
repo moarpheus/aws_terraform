3c40b7d741461feec4faf081b2a3d35456368c4b
JG <juris.gribuska@bt.com>
2021-09-21 13:22:10 +0300

    outputs in main

--- main.tf
+++ main.tf
@@ -63,3 +63,19 @@ module "vpc" {
   }
 
 }
+
+#############################################################################
+# OUTPUTS
+#############################################################################
+
+output "vpc_id" {
+  value = module.vpc.vpc_id
+}
+
+output "db_subnet_group" {
+  value = module.vpc.database_subnet_group
+}
+
+output "public_subnets" {
+  value = module.vpc.public_subnets
+}
